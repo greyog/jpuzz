a8a0233abf1fb5cdafe2d1441ff7eaa28f431da1
Mac Air <mac@Macs-MacBook-Air.local>
2018-05-12 01:04:36 +0300

    Initial commit

---
+++ android/src/main/java/com/greyogproducts/greyog/jpuzz/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.greyogproducts.greyog.jpuzz.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.greyogproducts.greyog.jpuzz.MainClass;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        initialize(new MainClass(), configuration);
+    }
+}
---
+++ core/src/main/java/com/greyogproducts/greyog/jpuzz/MainClass.java
@@ -0,0 +1,11 @@
+package com.greyogproducts.greyog.jpuzz;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class MainClass extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
